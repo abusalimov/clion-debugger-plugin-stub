66e3d68fe4bd5f49e48eef3dedbe98727c7f76a3
Eldar Abusalimov <Eldar.Abusalimov@jetbrains.com>
2023-07-04 12:25:16 +0200

    Add MyGDBSessionTest to showcase integration tests

---
+++ src/test/kotlin/com/example/helloplugin/MyGDBSessionTest.kt
@@ -0,0 +1,57 @@
+package com.example.helloplugin
+
+import com.intellij.openapi.application.PathManager
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.xdebugger.XDebuggerAssertions.assertCurrentPosition
+import com.intellij.xdebugger.XDebuggerTestUtil.toggleBreakpoint
+import com.jetbrains.cidr.CidrTestDataFixture
+import com.jetbrains.cidr.cpp.CPPTestCase
+import com.jetbrains.cidr.cpp.cmake.CMakeProjectFixture
+import com.jetbrains.cidr.cpp.execution.CMakeExecutionFixture
+import com.jetbrains.cidr.cpp.execution.debugger.gdb.CMakeGDBDebuggingFixture
+import com.jetbrains.cidr.cpp.toolchains.CPPEnvironment
+import com.jetbrains.cidr.execution.debugger.CidrDebuggingTestCase
+import com.jetbrains.cidr.execution.debugger.DebuggerDriverKind
+import org.junit.Test
+import java.io.File
+
+
+class MyGDBSessionTest :
+    CidrDebuggingTestCase<CMakeProjectFixture?, CMakeExecutionFixture?, CMakeGDBDebuggingFixture?, MyTestProjectMarkup>(
+        DebuggerDriverKind.GDB, MyTestProjectMarkup.TARGET_NAME, MyTestProjectMarkup.PROJECT_DIR
+    ) {
+
+    override fun createProjectFixture(): CMakeProjectFixture {
+        return CMakeProjectFixture(myTestDataFixture)
+    }
+
+    override fun createExecutionFixture(): CMakeExecutionFixture {
+        return CMakeExecutionFixture(myProjectFixture!!)
+    }
+
+    override fun createDebuggingFixture(): CMakeGDBDebuggingFixture {
+        return CMakeGDBDebuggingFixture(myExecutionFixture!!)
+    }
+
+    override fun createTestDataFixture(): CidrTestDataFixture {
+        val testData =
+            checkNotNull(PathManager.getResourceRoot(MyGDBSessionTest::class.java, "/projects")) { "testData" }
+        return CidrTestDataFixture(File(testData), false)
+    }
+
+    override fun createProjectMarkup(projectDir: VirtualFile): MyTestProjectMarkup {
+        return MyTestProjectMarkup(projectDir)
+    }
+
+    override fun getEnvironment(): CPPEnvironment {
+        return CPPTestCase.getTestCPPEnvironment()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testForceStepIntoFunctionWithNoSource() {
+        toggleBreakpoint(project, myProjectMarkup.FILE_MAIN, myProjectMarkup.LINE_BREAKPOINT)
+        startDebugSessionAndWaitForPause()
+        assertCurrentPosition(session, myProjectMarkup.FILE_MAIN, myProjectMarkup.LINE_BREAKPOINT)
+    }
+}
